5b8288bac0a2157a6c83e77d14cad2865190fa88
gabrieltjaeger <gabrieltjaeger@gmail.com>
2025-02-24 11:09:45 -0300

    :sparkles: feat(prisma-repositories): initial setup for repository implementation with prisma

---
+++ api/src/infra/database/repositories/prisma-associates-repository.ts
@@ -0,0 +1,63 @@
+import { Associate } from "@core/entities/associate";
+
+import {
+  AssociatesRepository,
+  AssociatesRepositoryFilterOptions,
+} from "@core/repositories/associates-repository";
+
+import { prisma } from "@infra/database/prisma-client";
+
+import { RepositoryQueryMode } from "@core/types/repository-query-mode";
+import { PrismaAssociateMapper } from "@infra/database/mappers/prisma-associate-mapper";
+
+export class PrismaAssociatesRepository implements AssociatesRepository {
+  async create(associate: Associate): Promise<void> {
+    await prisma.associate.create({
+      data: PrismaAssociateMapper.toPersistence(associate),
+    });
+  }
+
+  async update(associate: Associate): Promise<void> {
+    await prisma.associate.update({
+      where: { id: associate.id.value },
+      data: PrismaAssociateMapper.toPersistence(associate),
+    });
+  }
+
+  async find(
+    _: RepositoryQueryMode,
+    { id, email, phone }: AssociatesRepositoryFilterOptions
+  ): Promise<Associate | null> {
+    const associate = await prisma.associate.findUnique({
+      where: { id, email, phone },
+      include: { address: false },
+    });
+
+    return associate ? PrismaAssociateMapper.toEntity(associate) : null;
+  }
+
+  async list(
+    _: RepositoryQueryMode,
+    { page = 1, take = 10, fullName }: AssociatesRepositoryFilterOptions
+  ): Promise<Associate[] & { next?: number | null; prev?: number | null }> {
+    const [associates, count] = await Promise.all([
+      prisma.associate.findMany({
+        where: { fullName: { contains: fullName, mode: "insensitive" } },
+        skip: (page - 1) * take,
+        take,
+        include: { address: false },
+        orderBy: { fullName: "asc" },
+      }),
+      prisma.associate.count(),
+    ]);
+
+    return Object.assign(associates.map(PrismaAssociateMapper.toEntity), {
+      next: count > page * take ? page + 1 : null,
+      prev: page > 1 ? page - 1 : null,
+    });
+  }
+
+  async delete(associate: Associate): Promise<void> {
+    await prisma.associate.delete({ where: { id: associate.id.value } });
+  }
+}
---
+++ api/src/infra/database/repositories/prisma-mensalities-repository.ts
@@ -0,0 +1,67 @@
+import { prisma } from "@infra/database/prisma-client";
+
+import { RepositoryQueryMode } from "@core/types/repository-query-mode";
+
+import { Mensality } from "@core/entities/mensality";
+import {
+  MensalitiesRepository,
+  MensalitiesRepositoryFilterOptions,
+} from "@core/repositories/mensalities-repository";
+import { PrismaMensalityMapper } from "@infra/database/mappers/prisma-mensality-mapper";
+
+export class PrismaMensalitiesRepository implements MensalitiesRepository {
+  async create(mensality: Mensality): Promise<void> {
+    await prisma.mensality.create({
+      data: PrismaMensalityMapper.toPersistence(mensality),
+    });
+  }
+
+  async update(mensality: Mensality): Promise<void> {
+    await prisma.mensality.update({
+      where: { id: mensality.id.value },
+      data: PrismaMensalityMapper.toPersistence(mensality),
+    });
+  }
+
+  async delete(mensality: Mensality): Promise<void> {
+    await prisma.mensality.delete({ where: { id: mensality.id.value } });
+  }
+
+  async find(
+    _: RepositoryQueryMode,
+    { id, month, year }: MensalitiesRepositoryFilterOptions
+  ): Promise<Mensality | null> {
+    const mensality = await prisma.mensality.findUnique({
+      where: { id, month, year },
+      include: { payments: true },
+    });
+
+    return mensality ? PrismaMensalityMapper.toEntity(mensality) : null;
+  }
+
+  async list(
+    mode: RepositoryQueryMode,
+    { page = 1, take = 10, year }: MensalitiesRepositoryFilterOptions
+  ): Promise<Mensality[] & { next?: number | null; prev?: number | null }> {
+    const [mensalities, count] = await Promise.all([
+      prisma.mensality.findMany({
+        ...(year && { where: { year } }),
+        skip: (page - 1) * take,
+        take,
+
+        include: {
+          ...(mode === "expanded" && { payments: true }),
+          ...(mode === "deep" && {
+            payments: { include: { associate: true } },
+          }),
+        },
+      }),
+      prisma.mensality.count(),
+    ]);
+
+    return Object.assign(mensalities.map(PrismaMensalityMapper.toEntity), {
+      next: count > page * take ? page + 1 : null,
+      prev: page > 1 ? page - 1 : null,
+    });
+  }
+}

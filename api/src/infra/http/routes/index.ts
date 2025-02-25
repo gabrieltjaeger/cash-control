68eea7c8adc1a46ec9d42ba0bcaf9f777cabaa87
gabrieltjaeger <gabrieltjaeger@gmail.com>
2025-02-25 14:23:11 -0300

    :sparkles: feat(http-routes): register route endpoint for register-mensality controller

--- api/src/infra/http/routes/index.ts
+++ api/src/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { fetchAssociateController } from "@infra/http/controllers/fetch-associate";
 import { listAssociatesController } from "@infra/http/controllers/list-associates";
 import { registerAssociateController } from "@infra/http/controllers/register-associate";
+import { registerMensalityController } from "@infra/http/controllers/register-mensality";
 import { Router } from "express";
 
 export const router = Router();
@@ -8,3 +9,5 @@ export const router = Router();
 router.get("/associates", listAssociatesController);
 router.post("/associates", registerAssociateController);
 router.get("/associates/:id", fetchAssociateController);
+
+router.post("/mensalities", registerMensalityController);

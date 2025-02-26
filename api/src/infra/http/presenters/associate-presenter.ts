98f571a7a3205eaccdb4d4c77b2505be25b86a17
gabrieltjaeger <gabrieltjaeger@gmail.com>
2025-02-26 15:00:10 -0300

    :recycle: refactor(http-presenters): include other entities with their respective presenters in existing presenters

--- api/src/infra/http/presenters/associate-presenter.ts
+++ api/src/infra/http/presenters/associate-presenter.ts
@@ -1,15 +1,23 @@
 import { Associate, AssociateProps } from "@core/entities/associate";
 
+import { AddressPresenter } from "@infra/http/presenters/address-presenter";
+import { PaymentPresenter } from "@infra/http/presenters/payment-presenter";
+
 import { View } from "@infra/types/view";
 
 export class AssociatePresenter {
   static toDTO(associate?: Associate): View<AssociateProps> {
     if (associate)
       return {
-        id: associate.id,
+        id: associate.id.value,
         fullName: associate.fullName,
         email: associate.email,
         phone: associate.phone,
+        addressId: associate.addressId,
+        address: AddressPresenter.toDTO(associate.address ?? undefined),
+        payments: Array.from(associate.payments).map(([_, payment]) =>
+          PaymentPresenter.toDTO(payment)
+        ),
         createdAt: associate.createdAt,
         updatedAt: associate.updatedAt,
       };
--- api/src/infra/http/presenters/mensality-presenter.ts
+++ api/src/infra/http/presenters/mensality-presenter.ts
@@ -1,15 +1,20 @@
 import { Mensality, MensalityProps } from "@core/entities/mensality";
 
+import { PaymentPresenter } from "@infra/http/presenters/payment-presenter";
+
 import { View } from "@infra/types/view";
 
 export class MensalityPresenter {
   static toDTO(mensality?: Mensality): View<MensalityProps> {
     if (mensality)
       return {
-        id: mensality.id,
+        id: mensality.id.value,
         month: mensality.month,
         year: mensality.year,
         priceInCents: mensality.priceInCents,
+        payments: Array.from(mensality.payments).map(([_, payment]) =>
+          PaymentPresenter.toDTO(payment)
+        ),
         createdAt: mensality.createdAt,
         updatedAt: mensality.updatedAt,
       };

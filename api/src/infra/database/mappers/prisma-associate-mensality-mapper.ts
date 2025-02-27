9c5d2a53855706b657260493b7e05e07d0b8bf56
gabrieltjaeger <gabrieltjaeger@gmail.com>
2025-02-27 16:30:38 -0300

    :recycle: refactor(prisma-associate-mensality-mapper): add associate field to the toEntity function

--- api/src/infra/database/mappers/prisma-associate-mensality-mapper.ts
+++ api/src/infra/database/mappers/prisma-associate-mensality-mapper.ts
@@ -1,4 +1,6 @@
 import { AssociateMensality } from "@core/entities/associate-mensality";
+
+import { PrismaAssociateMapper } from "@infra/database/mappers/prisma-associate-mapper";
 import { PrismaMensalityMapper } from "@infra/database/mappers/prisma-mensality-mapper";
 
 import { CUID } from "@core/entities/types/CUID";
@@ -7,12 +9,16 @@ import { Prisma } from "@prisma/client";
 
 type PrismaAssociateMensality = Prisma.AssociateMensalityGetPayload<{}> & {
   mensality?: Prisma.MensalityGetPayload<{}>;
+  associate?: Prisma.AssociateGetPayload<{}>;
 };
 
 export class PrismaAssociateMensalityMapper {
   static toEntity(raw: PrismaAssociateMensality): AssociateMensality {
     return AssociateMensality.create({
       associateId: new CUID(raw.associateId),
+      ...(raw.associate && {
+        associate: PrismaAssociateMapper.toEntity(raw.associate),
+      }),
       mensalityId: new CUID(raw.mensalityId),
       ...(raw.mensality && {
         mensality: PrismaMensalityMapper.toEntity(raw.mensality),

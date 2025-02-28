d9c2ac5ed160b2ce8067b29433ac3d6ce6b72e30
gabrieltjaeger <gabrieltjaeger@gmail.com>
2025-02-28 15:38:35 -0300

    :bug: fix(payment-routes): add missing `RegisterPayment` route

--- api/src/infra/http/routes/payments/index.ts
+++ api/src/infra/http/routes/payments/index.ts
@@ -1,3 +1,7 @@
+import { registerPaymentController } from "@infra/http/controllers/payments/register-payment";
+
 import { Router } from "express";
 
 export const paymentsRouter = Router();
+
+paymentsRouter.post("/", registerPaymentController);

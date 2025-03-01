28a83e834a07edca20f603b2ac32e8a06540b0bc
gabrieltjaeger <gabrieltjaeger@gmail.com>
2025-03-01 14:46:57 -0300

    :sparkles: feat(list-payments): create `listPaymentsController`, its http route, and register it in swagger docs

--- api/src/infra/http/routes/payments/index.ts
+++ api/src/infra/http/routes/payments/index.ts
@@ -1,7 +1,9 @@
+import { listPaymentsController } from "@infra/http/controllers/payments/list-payments";
 import { registerPaymentController } from "@infra/http/controllers/payments/register-payment";
 
 import { Router } from "express";
 
 export const paymentsRouter = Router();
 
+paymentsRouter.get("/", listPaymentsController);
 paymentsRouter.post("/", registerPaymentController);

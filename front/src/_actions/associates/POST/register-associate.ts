0acfb1e726696201d32eaaa4ec1bd367ca914971
gabrieltjaeger <gabrieltjaeger@gmail.com>
2025-03-05 14:46:29 -0300

    :recycle: refactor(server-actions): move actions to src, and update register-associate to use fetch service

---
+++ front/src/_actions/associates/POST/register-associate.ts
@@ -0,0 +1,18 @@
+"use server";
+
+import fetchService from "@/services/fetch-service";
+
+interface RegisterAssociateRequest {
+  fullName: string;
+  email: string;
+  phone: string;
+}
+
+export async function registerAssociate(data: RegisterAssociateRequest) {
+  const response = fetchService.POST<RegisterAssociateRequest>({
+    url: `${process.env.API_URL}/associates`,
+    data,
+  });
+
+  return response;
+}

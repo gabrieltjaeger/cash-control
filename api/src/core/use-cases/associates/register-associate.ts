4624776676eaf99b40cbcf8baf090fcfbd85f8ec
gabrieltjaeger <gabrieltjaeger@gmail.com>
2025-03-10 16:34:32 -0300

    :sparkles: feat(domain-errors): use domain errors in register associate in case of existing resources

--- api/src/core/use-cases/associates/register-associate.ts
+++ api/src/core/use-cases/associates/register-associate.ts
@@ -1,6 +1,8 @@
 import { Associate } from "@core/entities/associate";
 import { AssociatesRepository } from "@core/repositories/associates-repository";
 
+import { ExistingResourceError } from "@core/errors/existing-resource-error";
+
 interface RegisterAssociateUseCaseRequest {
   fullName: string;
   email: string;
@@ -26,6 +28,24 @@ export class RegisterAssociateUseCase {
     phone,
     address,
   }: RegisterAssociateUseCaseRequest): Promise<void> {
+    const existingAssociateEmail = await this.associatesRepository.find(
+      "minimal",
+      { email }
+    );
+
+    if (existingAssociateEmail) {
+      throw new ExistingResourceError("Associate", "email");
+    }
+
+    const existingAssociatePhone = await this.associatesRepository.find(
+      "minimal",
+      { phone }
+    );
+
+    if (existingAssociatePhone) {
+      throw new ExistingResourceError("Associate", "phone");
+    }
+
     const associate = Associate.create({ fullName, email, phone });
 
     await this.associatesRepository.create(associate);

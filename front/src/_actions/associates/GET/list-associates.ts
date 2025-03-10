64ddfe151a4f07508ce684e49836604f10a514e1
gabrieltjaeger <gabrieltjaeger@gmail.com>
2025-03-10 16:56:37 -0300

    :sparkles: feat(actions): create get list associates action

---
+++ front/src/_actions/associates/GET/list-associates.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import fetchService from "@/services/fetch-service";
+
+interface ListAssociatesRequest {
+  page: number;
+  name?: string;
+}
+
+export async function listAssociates(data: ListAssociatesRequest) {
+  const response = fetchService.GET({
+    url: `${process.env.API_URL}/associates?page=${data.page}${
+      data.name ? `&name=${data.name}` : ""
+    }`,
+    data,
+  });
+
+  return response;
+}

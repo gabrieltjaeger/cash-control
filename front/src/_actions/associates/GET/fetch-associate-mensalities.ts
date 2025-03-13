41a0752ef1b7ee17382defcf22c708a36a28d468
gabrieltjaeger <gabrieltjaeger@gmail.com>
2025-03-13 10:17:33 -0300

    :sparkles: feat(actions): create missing actions related to associates

---
+++ front/src/_actions/associates/GET/fetch-associate-mensalities.ts
@@ -0,0 +1,18 @@
+"user server";
+
+import fetchService from "@/services/fetch-service";
+
+interface FetchAssociateMensalitiesRequest {
+  id: string;
+  year: number;
+}
+
+export async function fetchAssociateMensalities(
+  data: FetchAssociateMensalitiesRequest
+) {
+  const response = fetchService.GET({
+    url: `${process.env.API_URL}/associates/${data.id}/mensalities?year=${data.year}`,
+  });
+
+  return response;
+}

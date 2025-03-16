63f767297ebf9b14e9c78287212c9a0569f76a89
gabrieltjaeger <gabrieltjaeger@gmail.com>
2025-03-15 23:20:35 -0300

    :recycle: refactor(DTOs): update `AssociateDTO` to include address, payments, and mensalities

--- front/src/interfaces/dtos/associate-dto.ts
+++ front/src/interfaces/dtos/associate-dto.ts
@@ -1,6 +1,16 @@
+import { AddressDTO } from "@/interfaces/dtos/address-dto";
+import { MensalityDTO } from "@/interfaces/dtos/mensality-dto";
+import { PaymentDTO } from "@/interfaces/dtos/payment-dto";
+
 export interface AssociateDTO {
   id: string;
   fullName: string;
   email: string;
   phone: string;
+  addressId: string;
+  address: AddressDTO | null;
+  payments: PaymentDTO[] | null;
+  mensalities: MensalityDTO[] | null;
+  createdAt: Date;
+  updatedAt: Date | null;
 }

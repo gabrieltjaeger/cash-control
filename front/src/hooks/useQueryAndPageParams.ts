5972c6d40a43dbfba4fe5d291524fbe2aac4a7bd
gabrieltjaeger <gabrieltjaeger@gmail.com>
2025-03-27 16:53:30 -0300

    :recycle: refactor(hooks): warp setPage function

--- front/src/hooks/useQueryAndPageParams.ts
+++ front/src/hooks/useQueryAndPageParams.ts
@@ -1,12 +1,19 @@
 import { useState } from "react";
 
 export function useQueryAndPageParams() {
-  const [page, setPage] = useState(1);
+  const [page, _setPage] = useState(1);
   const [query, _setQuery] = useState("");
 
-  const setQuery = (query: string) => {
-    _setQuery(query);
+  const setQuery = (value: string) => {
+    _setQuery(value);
     setPage(1);
+  };
+
+  const setPage = (value: number) => {
+    if (value < 1) {
+      value = 1;
+    }
+    _setPage(value);
   };
 
   return {

02523db6653da1b0eb8de7a100d0b4f5267b83b7
gabrieltjaeger <gabrieltjaeger@gmail.com>
2025-03-28 20:25:52 -0300

    :sparkles: feat(list-associates): add profile link button for each associate and implement motion animation

--- front/src/app/associates/components/ListAssociates.tsx
+++ front/src/app/associates/components/ListAssociates.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import { LoadingSpinner } from "@/components/LoadingSpinner";
 import PaginationController from "@/components/PaginationController";
 import SearchQueryInput from "@/components/SearchQueryInput";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -18,6 +21,9 @@ import {
 } from "@/components/ui/table";
 import { useListAssociates } from "@/hooks/associates/useListAssociates";
 import { useQueryAndPageParams } from "@/hooks/useQueryAndPageParams";
+import { Forward } from "lucide-react";
+import { motion } from "motion/react";
+import Link from "next/link";
 import { Suspense, memo } from "react";
 
 const ListAssociates = memo(function ListAssociates() {
@@ -28,58 +34,77 @@ const ListAssociates = memo(function ListAssociates() {
   });
 
   return (
-    <Card className="overflow-x-hidden col-span-2 lg:col-span-3">
-      <CardHeader>
-        <CardTitle className="text-2xl font-bold">List Associates</CardTitle>
-        <CardDescription>
-          Search for associates and view their information.
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="h-full flex flex-col justify-start items-center gap-2">
-        <Suspense fallback={<LoadingSpinner />}>
-          <SearchQueryInput setQuery={setQuery} />
-          {isLoading && (
-            <div className="flex items-center justify-center h-full">
-              <LoadingSpinner />
-            </div>
-          )}
-          {!isLoading && data.associates.length === 0 && (
-            <div className="flex items-center justify-center h-full">
-              No associates found.
-            </div>
-          )}
-          {!isLoading && data.associates.length > 0 && (
-            <>
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead>Name</TableHead>
-                    <TableHead>E-mail</TableHead>
-                    <TableHead>Phone</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {!!data.associates &&
-                    data.associates.map((associate) => (
-                      <TableRow key={associate.id}>
-                        <TableCell>{associate.fullName}</TableCell>
-                        <TableCell>{associate.email}</TableCell>
-                        <TableCell>{associate.phone}</TableCell>
-                      </TableRow>
-                    ))}
-                </TableBody>
-              </Table>
-              <PaginationController
-                page={page}
-                setPage={setPage}
-                next={data.pagination.next}
-                prev={data.pagination.prev}
-              />
-            </>
-          )}
-        </Suspense>
-      </CardContent>
-    </Card>
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5, delay: 0.2 }}
+      className="overflow-x-hidden col-span-2 lg:col-span-3"
+    >
+      <Card>
+        <CardHeader>
+          <CardTitle>List Associates</CardTitle>
+          <CardDescription>
+            Search for associates and view their information.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="h-full flex flex-col justify-start items-center gap-2">
+          <Suspense fallback={<LoadingSpinner />}>
+            <SearchQueryInput setQuery={setQuery} />
+            {isLoading && (
+              <div className="flex items-center justify-center h-full">
+                <LoadingSpinner />
+              </div>
+            )}
+            {!isLoading && data.associates.length === 0 && (
+              <div className="flex items-center justify-center h-full">
+                No associates found.
+              </div>
+            )}
+            {!isLoading && data.associates.length > 0 && (
+              <>
+                <Table>
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead>Name</TableHead>
+                      <TableHead>E-mail</TableHead>
+                      <TableHead>Phone</TableHead>
+                      <TableHead>Profile</TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    {!!data.associates &&
+                      data.associates.map((associate) => (
+                        <TableRow key={associate.id}>
+                          <TableCell>{associate.fullName}</TableCell>
+                          <TableCell>{associate.email}</TableCell>
+                          <TableCell>{associate.phone}</TableCell>
+                          <TableCell>
+                            <Link href={`/associates/${associate.id}`}>
+                              <Button
+                                variant="link"
+                                size="sm"
+                                className="hover:cursor-pointer hover:text-blue-500"
+                              >
+                                <Forward />
+                              </Button>
+                            </Link>
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                  </TableBody>
+                </Table>
+                <PaginationController
+                  page={page}
+                  setPage={setPage}
+                  next={data.pagination.next}
+                  prev={data.pagination.prev}
+                />
+              </>
+            )}
+          </Suspense>
+        </CardContent>
+      </Card>
+    </motion.div>
   );
 });
 

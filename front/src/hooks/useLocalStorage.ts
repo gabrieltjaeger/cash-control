fb1c1e2b49e97d30a867acf299042e4e445fe2b7
gabrieltjaeger <gabrieltjaeger@gmail.com>
2025-11-16 20:32:20 -0300

    :wastebasket: remove(hooks): delete `useLocalStorage` hook

--- front/src/hooks/useLocalStorage.ts
+++
@@ -1,9 +0,0 @@
-export function useLocalStorage() {
-  const setInLocalStorage = (key: string, value: string) => {
-    localStorage.setItem(key, value);
-  };
-  const getFromLocalStorage = (key: string) => {
-    return localStorage.getItem(key);
-  };
-  return { setInLocalStorage, getFromLocalStorage };
-}
